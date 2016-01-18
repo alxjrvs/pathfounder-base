0270a4f18d62bad6e64ddbb6f81e851d96be117a
Alex Jarvis <alxjrvs@gmail.com>
2016-01-17 21:49:02 -0500

    Remote StatBlock Serializer

--- app/serializers/stat_block_serializer.rb
+++
@@ -1,9 +0,0 @@
-class StatBlockSerializer < ActiveModel::Serializer
-  StatBlock::STATS.each do |st|
-    attribute st
-
-    define_method(st) do
-      StatSerializer.new(object.send(st))
-    end
-  end
-end
